b84487687ac58eb2228b7e66bbbb218f23450f46
Aygen Yucel <aygenyucel@outlook.com>
2022-07-20 13:04:51 +0300

    files changed

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -105,8 +105,14 @@ function Work() {
 
       </motion.div>
 
+      {/* <div class="custom-shape-divider-bottom-1657991521">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M602.45,3.86h0S572.9,116.24,281.94,120H923C632,116.24,602.45,3.86,602.45,3.86Z" class="shape-fill"></path>
+        </svg>
+      </div> */}
 
     </>
+    
   )  
 }
 
