74ec989d5ed916d8f2cb1739480dcf8168a60399
Aygen Yucel <aygenyucel@outlook.com>
2022-07-20 13:16:21 +0300

    files changed

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,10 +1,6 @@
 import { color } from '@mui/system';
 import React from 'react';
 import { NavigationDots, SocialMedia } from '../components';
-// import { useLocation } from 'react-router-dom';
-
-// const location = useLocation();
-// const { pathname } = location;
 
 
 const AppWrap = (Component, idName, classNames) => function HOC() {
@@ -19,8 +15,8 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
                 <p className='p-text'>@2022 AYGEN</p>
                 <p className='p-text'>All rights reserved</p>
             </div>
-            {/* <div className = {`divider divider-${pathname}`}> */}
-            <div className = {`divider`}>
+            
+            {/* <div className = {`divider`}>
               <svg
                 data-name="Layer 1"
                 xmlns="http://www.w3.org/2000/svg"
@@ -30,7 +26,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
               <path
                 d="M602.45,3.86h0S572.9,116.24,281.94,120H923C632,116.24,602.45,3.86,602.45,3.86Z"
                 class="shape-fill"
-              ></path>
+              ></path> */}
             </svg>
           </div>
         </div>
